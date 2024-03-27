147bda98455fd7abb5a9ae01d6a2630e5272ea1d
ojhsh7 <163072092+ojhsh7@users.noreply.github.com>
2024-03-27 16:21:14 +0900

    Click 1

---
+++ Assets/2_Scripts/NoteManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteManager : MonoBehaviour
+{
+    public static NoteManager Instance;
+        private void Awake()  
+    {
+        Instance = this;
+    }
+    public void OnInput(KeyCode keyCode)
+    {
+        Debug.Log("Keycode = " + keyCode);
+    }
+}
