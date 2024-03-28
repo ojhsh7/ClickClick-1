96e10822b9a1e65ce7e9ca2cd87a3b92b121fe7c
ojhsh7 <163072092+ojhsh7@users.noreply.github.com>
2024-03-28 12:38:27 +0900

    새로운 클릭이

---
+++ Assets/2_Scripts/Note/NoteGroup.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteGroup : MonoBehaviour
+{
+    [SerializeField] private int noteMaxNum = 5;
+    [SerializeField] private GameObject notePrefab;
+    [SerializeField] private GameObject noteSpawn;
+    [SerializeField] private float noteGap = 6f;
+
+    private List<Note> noteList = new List<Note>();
+    void Start()
+    {
+        for (int i = 0; i < noteMaxNum; i++)
+        {
+
+            GameObject noteGameObj = Instantiate(notePrefab);
+            noteGameObj.transform.SetParent(noteSpawn.transform);
+
+
+            noteGameObj.transform.localPosition = Vector3.up * noteList.Count * noteGap;
+            Debug.Log("noteGameObj.transform.position ;  " + noteGameObj.transform.position );
+            Note note = noteGameObj.GetComponent<Note>();
+
+            noteList.Add(note);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
